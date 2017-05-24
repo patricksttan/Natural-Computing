6f0e124b3fe2bacc2f0f0446805ca6e8aea6667e
Patrick Tan <patricksttan@gmail.com>
2017-05-24 15:20:10 +0200

    Add files via upload

--- get_problem.py
+++ get_problem.py
@@ -22,21 +22,22 @@ def get_problem(file):
         
     f.close()
     
-    for i in range (0, len(knap)-1):
+    for i in range (0, len(knap)):
         knap[i] = knap[i].split(' ')
-        knap[i] = knap[i][1:-1]
-        
-    knap[len(knap)-1] = knap[len(knap)-1][1:]
-    knap[len(knap)-1] = knap[len(knap)-1].split(' ')
+        if knap[i][0] == '':
+            knap[i] = knap[i][1:]
+        if knap[i][len(knap[i]) - 1] == '\r\n':
+            knap[i] = knap[i][:-1]
     
     number_of_objects = int(knap[0][0])
+    number_of_dimensions = int(knap[0][1])
     knapsack_capacities = []
-    
-    for i in range(0, len(knap[len(knap)-1])):
-        knapsack_capacities.append(int(knap[len(knap)-1][i]))
-        
-    knap = knap[1:-1]
+
+    knap = knap[:-1]
     knap = [int(item) for sublist in knap for item in sublist]
+    
+    for i in range(0, number_of_dimensions):
+        knapsack_capacities.append(knap[len(knap) - i - 1])
     
     items = []
     
